1a653549aa823f2d02c9b5024f9e194132645fb9
Farrukh Tuheed Khan <farrukh.tuheed@northbaysolutions.net>
2013-09-06 11:24:43 +0500

    Calculator with Unit Test

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,36 @@
+ 
+public class Calculator {
+ 
+	public int add(int num1, int num2){
+
+		//int sum;
+		//sum = num1 + num2;
+		return num1 + num2;
+	}
+
+	public double sub(double num1, double num2){
+
+ 
+		double sum;
+		sum = num1 - num2;
+		return sum;
+	}
+ 
+	public double multi(double num1, double num2){
+
+ 
+		double sum;
+		sum = num1 * num2;
+		return sum;
+
+	}
+ 
+	public double div(double num1, double num2){
+
+ 
+		double sum;
+		sum = num1 / num2;
+		return sum;
+
+	}
+}
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,45 @@
+
+import junit.framework.TestCase;
+import static org.junit.Assert.*;
+
+public class TestCalculator extends TestCase {
+
+	public TestCalculator (String name){
+		super(name);
+	}
+	
+
+	Calculator calculator; /** @throws java.lang.Exception */
+
+	protected void setUp() throws Exception {
+		calculator = new Calculator(); 
+
+	} //** @throws java.lang.Exception *// 
+	
+	protected void tearDown() throws Exception {
+
+	} 
+	
+	public void testAdd() {
+		assertEquals(calculator.add(2, 2),4);
+		assertEquals(calculator.add(2, 4), 6);
+		
+		
+
+		
+	}
+	
+	public void testSub() {
+		assertEquals(calculator.sub(7.0, 7.0), 0.0);
+		
+	}
+	
+	public void testMulti() {
+		assertEquals(calculator.multi(7.0, 7.0), 49.0);
+		
+	}
+	
+	public void testDiv() {
+		assertEquals(calculator.div(7.0, 7.0), 1.0);
+	}
+}
