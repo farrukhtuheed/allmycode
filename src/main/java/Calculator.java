20e786a90ca4fca6efc2f6a5a9853e49a01051d7
Farrukh Tuheed Khan <farrukh.tuheed@northbaysolutions.net>
2013-09-06 17:45:09 +0500

    add if conditions in class file

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,34 +3,69 @@ public class Calculator {
  
 	public int add(int num1, int num2){
 
-		//int sum;
-		//sum = num1 + num2;
-		return num1 + num2;
+		int sum;
+		sum = num1 + num2;
+		if ( sum == 6 || sum == 7 || sum == 8){
+			
+			System.out.println("Sum is :" + sum);
+		}
+		
+		/*while (sum >= 7 && sum == 6) {
+			
+			System.out.println("Sum is greater than :" + sum);
+			int rst = sum - 1;
+			System.out.println("Result is greater than :" + rst);
+		}	
+		*/
+		
+		return sum;
 	}
 
 	public double sub(double num1, double num2){
 
  
-		double sum;
-		sum = num1 - num2;
-		return sum;
+		double result;
+		result = num1 - num2;
+		if(result== 0.0 || result == 9.5 || result == 10.5 || result == 7.5){
+			
+			System.out.println("Subtraction Result is " + result);
+			
+		}
+		
+		
+		
+		return result;
 	}
  
 	public double multi(double num1, double num2){
 
- 
-		double sum;
-		sum = num1 * num2;
-		return sum;
+		
+		double result;
+		result = num1 * num2;
+		if(result==9.0 || result == 49.0 || result == 1.0 || result ==9.0){
+			
+			System.out.println("Multiplication Result is " + result);
+			
+		}
+		
+		return result;
 
 	}
  
 	public double div(double num1, double num2){
 
  
-		double sum;
-		sum = num1 / num2;
-		return sum;
+		double result;
+		result = num1 / num2;
+		if(result==5.0 || result == 1.0 || result == 2.0 || result == 3.0){
+			
+			System.out.println("Division Result is " + result);
+			
+		}
+		
+		return result;
 
 	}
+	
+
 }
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,6 +1,5 @@
 
 import junit.framework.TestCase;
-import static org.junit.Assert.*;
 
 public class TestCalculator extends TestCase {
 
@@ -21,25 +20,42 @@ public class TestCalculator extends TestCase {
 	} 
 	
 	public void testAdd() {
-		assertEquals(calculator.add(2, 2),4);
+	
 		assertEquals(calculator.add(2, 4), 6);
-		
-		
-
-		
+		assertEquals(calculator.add(2, 5), 7);
+		assertEquals(calculator.add(2, 6),8);
 	}
 	
 	public void testSub() {
 		assertEquals(calculator.sub(7.0, 7.0), 0.0);
+		assertEquals(calculator.sub(14.0, 7.0), 7.0);
+		assertEquals(calculator.sub(17.0, 7.5), 9.5);
+		assertEquals(calculator.sub(18.0, 7.5), 10.5);
+
+		
 		
 	}
 	
 	public void testMulti() {
 		assertEquals(calculator.multi(7.0, 7.0), 49.0);
+		assertEquals(calculator.multi(1.0, 1.0), 1.0);
+		assertEquals(calculator.multi(2.0, 2.0), 4.0);
+		assertEquals(calculator.multi(3.0, 3.0), 9.0);
+		assertEquals(calculator.multi(10.0, 5.0), 50.0);
+
 		
 	}
 	
 	public void testDiv() {
 		assertEquals(calculator.div(7.0, 7.0), 1.0);
+		assertEquals(calculator.div(14.0, 7.0), 2.0);
+		assertEquals(calculator.div(21.0, 7.0), 3.0);
+		assertEquals(calculator.div(28.0, 7.0), 4.0);
+		assertEquals(calculator.div(35.0, 7.0), 5.0);
+
+		
 	}
+	
+	
+	
 }
