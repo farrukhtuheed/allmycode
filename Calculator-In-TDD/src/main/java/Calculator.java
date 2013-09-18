c2515b4bcbc31965a15ed5296266d8e0bbf552fe
Farrukh Tuheed Khan <farrukh.tuheed@northbaysolutions.net>
2013-09-18 15:58:30 +0500

    TDD-Code

--- Calculator-In-TDD/src/main/java/Calculator.java
+++ Calculator-In-TDD/src/main/java/Calculator.java
@@ -1,30 +1,27 @@
  
 public class Calculator {
  
-	public int add(int num1, int num2){
+/*	public int add(int num1, int num2){
 
 		int sum;
 		sum = num1 + num2;
 
-		if ( sum == 6 || sum == 7 || sum == 8){
+		if ( sum == 6 || sum == 7 || sum == 8 || sum == 9 || sum == 10 ){
 			
 			System.out.println("Sum is :" + sum);
 		}
 		
-	
-		if ( sum == 7 ){
-			System.out.println("Sum is 7");
-		}
-		
 		return sum;
 	}
+	
+
 
 	public double sub(double num1, double num2){
 
  
 		double result;
 		result = num1 - num2;
-		if(result== 0.0 || result == 9.5 || result == 10.5 || result == 7.5){
+		if(result== 0.0 || result == 7.0 || result == 9.5 || result == 10.5 || result == 11.5){
 			
 			System.out.println("Subtraction Result is " + result);
 			
@@ -40,7 +37,7 @@ public class Calculator {
 		
 		double result;
 		result = num1 * num2;
-		if(result==9.0 || result == 49.0 || result == 1.0 || result ==9.0){
+		if( result == 1.0 || result == 4.0 || result ==9.0 || result==49.0 || result == 50.0){
 			
 			System.out.println("Multiplication Result is " + result);
 			
@@ -55,13 +52,24 @@ public class Calculator {
  
 		double result;
 		result = num1 / num2;
-		if(result==5.0 || result == 1.0 || result == 2.0 || result == 3.0){
+		if(result == 1.0 || result == 2.0 || result == 3.0 || result==4.0 || result==5.0){
 			
 			System.out.println("Division Result is " + result);
 			
 		}
 		
 		return result;
+
+	}*/
+	
+	public int tab(int b){
+		//int a =0;
+		for (int i=0 ; i <5 ; i++){
+			
+			System.out.println("Number Sequence : " + i);
+			//a= i;
+		}
+return b;
 
 	}
 	
--- Calculator-In-TDD/src/test/java/TestCalculator.java
+++ Calculator-In-TDD/src/test/java/TestCalculator.java
@@ -19,35 +19,43 @@ public class TestCalculator extends TestCase {
 
 	} 
 	
-	public void testAdd() {
+/*	public void testAdd() {
 	
 		assertEquals(calculator.add(2, 4), 6);
 		assertEquals(calculator.add(2, 5), 7);
-		assertEquals(calculator.add(2, 6),8);
-		assertEquals(calculator.add(2, 5), 7);
+		assertEquals(calculator.add(2, 6), 8);
+		assertEquals(calculator.add(4, 5), 9);
+		assertEquals(calculator.add(5, 5), 10);
 		
-		
-
+		//out of if condition 
+		assertEquals(calculator.add(15, 5), 20);
 		
 	}
 	
+		
 	public void testSub() {
 		assertEquals(calculator.sub(7.0, 7.0), 0.0);
 		assertEquals(calculator.sub(14.0, 7.0), 7.0);
 		assertEquals(calculator.sub(17.0, 7.5), 9.5);
 		assertEquals(calculator.sub(18.0, 7.5), 10.5);
-
+		assertEquals(calculator.sub(18.0, 6.5), 11.5);
+		
+		//out of if condition 
+		assertEquals(calculator.sub(20.0, 6.0), 14.0);
 		
 		
 	}
 	
 	public void testMulti() {
-		assertEquals(calculator.multi(7.0, 7.0), 49.0);
+	
 		assertEquals(calculator.multi(1.0, 1.0), 1.0);
 		assertEquals(calculator.multi(2.0, 2.0), 4.0);
 		assertEquals(calculator.multi(3.0, 3.0), 9.0);
+		assertEquals(calculator.multi(7.0, 7.0), 49.0);
 		assertEquals(calculator.multi(10.0, 5.0), 50.0);
-
+		
+		//out of if condition 
+		assertEquals(calculator.multi(20.0, 6.0), 120.0);
 		
 	}
 	
@@ -58,9 +66,16 @@ public class TestCalculator extends TestCase {
 		assertEquals(calculator.div(28.0, 7.0), 4.0);
 		assertEquals(calculator.div(35.0, 7.0), 5.0);
 
-		
+		//out of if condition 
+		assertEquals(calculator.div(90.0, 3.0), 30.0);
 	}
+	*/
 	
+	public void testTab(){
+			//for(int i=0; i<5;i++)
+			assertEquals(calculator.tab(3),3);
+			
+	}
 	
 	
 }
